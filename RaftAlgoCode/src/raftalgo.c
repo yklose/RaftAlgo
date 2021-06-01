af85140b8869ef363539424fe707ae3fe3759faf
Tim Schröder <tim.schroeder@campus.tu-berlin.de>
2021-06-01 11:38:31 +0200

    cast float

--- RaftAlgoCode/src/raftalgo.c
+++ RaftAlgoCode/src/raftalgo.c
@@ -162,7 +162,7 @@ int main (void) {
 							}
 						//	printf("INCREASE ACCEPT COUNTER\n");
 						//	accept_counter += 1; //Nur wenn noch nicht in Liste
-							if ((accept_counter./num_nodes)>0.5) { //MAJORITY?
+							if (((float)accept_counter/num_nodes)>0.5) { //MAJORITY?
 								printf("I AM LEADER\n");
 								//CHANGE TO LEADER
 								state = set_state_leader();
