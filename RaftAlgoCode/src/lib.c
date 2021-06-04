437b27b35727d746cdd1f10bd133b0a6ee7e4ead
Yannick Klose <yannick.klose@icloud.com>
2021-06-04 10:25:48 +0200

    update

--- RaftAlgoCode/src/lib.c
+++ RaftAlgoCode/src/lib.c
@@ -206,7 +206,8 @@ int get_checksum_from_msg(char *msg){
 }
 
 int get_int_type_from_msg(char *msg){
-        char type = msg[0];
+        char type[1];
+        type[0] = msg[0];
 	return convert_char_to_int(type);
 }
 
