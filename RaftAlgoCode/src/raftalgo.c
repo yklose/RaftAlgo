7f4911c266cc155682094d143bf85a911350c787
Tim Schröder <tim.schroeder@campus.tu-berlin.de>
2021-07-05 09:53:26 +0200

    changes

--- RaftAlgoCode/src/raftalgo.c
+++ RaftAlgoCode/src/raftalgo.c
@@ -48,8 +48,8 @@ int main (void) {
 	cc1200_reg_write(PKT_CFG0, 0x01);			
 	cc1200_reg_write(PKT_LEN, max_packet_len);
 	// initialize generated numbers
-
-	cc1200_reg_write(0x2F80, 0x7F); //activate random numbers
+	cc1200_reg_write(RNDGEN, 0x01); //activate random numbers
+	//cc1200_reg_write(RNDGEN, 0x7F); //activate random numbers
 	cc1200_cmd(SNOP);
 	cc1200_reg_read(RNDGEN, &id);
 	printf("RANDOM ID: %d\n", id);
